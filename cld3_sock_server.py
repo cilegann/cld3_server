ea56b8b408001afe4ad0417c8040f19d6311c24b
cilegann <s571991@gmail.com>
2019-03-17 17:12:15 +0800

    add UnicodeDecodeerror retry mechanism

--- cld3_sock_server.py
+++ cld3_sock_server.py
@@ -4,11 +4,13 @@ HOST = '0.0.0.0'
 PORT = 8886 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse tcp
+#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse tcp
+# sock.settimeout(10)
+# sock.settimeout(None)
 sock.bind((HOST, PORT))
-sock.listen(5)
+sock.listen(7)
 print("CLD3 server is running at "+HOST+":"+str(PORT))
-# lock = threading.Lock()
+#lock = threading.Lock()
 
 class CLD3Server(threading.Thread):
     def __init__(self, socket, adr):
@@ -20,7 +22,8 @@ class CLD3Server(threading.Thread):
         print("Connected: "+str(self.address))
         while True:
             try:
-                request = self.socket.recv(1024).decode('utf-8')
+                request = self.socket.recv(4096)
+                request=request.decode('utf-8')
                 if not request:
                     break
                 print("Text in: "+request)
@@ -29,8 +32,12 @@ class CLD3Server(threading.Thread):
                 client.send(str.encode(result))
                 print()
                 #lock.release()
-            except socket.timeout:
+            except socket.timeout as e:
+                print(str(e))
                 break
+            except UnicodeDecodeError as e:
+                print("Decode Error")
+                client.send(str.encode("X"))
         self.socket.close()
         print("Disconnect")
 
@@ -38,4 +45,5 @@ if __name__ == "__main__":
     while True:
         (client, adr) = sock.accept()
         CLD3Server(client, adr).start()
+    sock.close()
 
--- remoteCld3.py
+++ remoteCld3.py
@@ -1,16 +1,21 @@
+
 import socket
+import time
 class cld3():
     def __init__(self):
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     def establishConn(self,ip,port):
         server=(ip,port)
         print("Connected to "+ip)
         self.__sock.connect(server)
+        self.__sock.settimeout(None)
 
     def getLang(self,t):
-        self.__sock.send(str.encode(t))
+        self.__sock.send(t.encode(encoding='utf-8'))
         result=self.__sock.recv(1024).decode()
+        while result=="X":
+            self.__sock.send(t.encode(encoding='utf-8'))
+            result=self.__sock.recv(1024).decode()
         return result
 
     def closeConn(self):
