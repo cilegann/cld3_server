eee610073a0fbaad484cb2ebd2fd6d3b665b9519
Sean Chiu <s571991@gmail.com>
2019-04-15 21:43:45 +0800

    Update clds_client_test.py

--- clds_client_test.py
+++ clds_client_test.py
@@ -1,17 +1,7 @@
-# from remoteCld3 import establishConn,getLang,closeConn
-
-# s=establishConn("140.112.26.127",8886)
-
-# texts=["hello how are you","I am fine","fuck you","You are my flower"]
-# for t in texts:
-#     print(getLang(s,t))
-
-# closeConn(s)
-
 import remoteCld3
 import time
 cld=remoteCld3.cld3()
-cld.establishConn("140.112.26.127",8886)
+cld.establishConn("your.ip.addr.here",portHere)
 for i in range(10000):
     print(cld.getLang("How are you"))
 cld.closeConn()
